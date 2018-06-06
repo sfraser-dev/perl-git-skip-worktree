56d4317e4d9e94b9a5a2f9184fae3bc9688512cd
Stewart Fraser <stewart.fraser@f-e-t.com>
2018-06-06 13:10:49 +0100

    let the user add file to ignore locally (.user and .suo)

--- worktreeSkip.pl
+++ worktreeSkip.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl -w
+# run this after a clean and then a rebuild all
 
 # runs with Strawberry Perl: http://strawberryperl.com/
 
@@ -10,14 +11,15 @@ use File::Basename;
 use Cwd;
 use POSIX qw(floor);
 
+my @content;
+
 my $theFile= "worktreeSkipFiles.txt";
 system("git diff --name-only >$theFile");
-
 # read each line from the file into an array
 open my $handleIn, '<', $theFile or die "Could not open file '$theFile' $!";
 chomp (my @linesIn = <$handleIn>);
 close $handleIn;
-# process each line from the array
+# process each line from the array (add each file to the "worktree skip" record)
 foreach my $line (@linesIn) {
     chomp($line);
     say $line;
@@ -25,4 +27,26 @@ foreach my $line (@linesIn) {
     system("git update-index --skip-worktree \"$line\"");
 }
 
+# find all files (that user wishes to ignore locally) from current and sub directories
+find( \&filesWanted, '.'); 
+# process each line from the array (add each file to the "worktree skip" record)
+foreach my $line (@content) {
+    chomp($line);
+    # remove the "./" at the start of each line (indicating a skipped worktree file)
+    my $n=2;
+    $line =~ s/^.{$n}//s;
+    say $line;
+    # encase filename / filepath in quotes to handle spaces
+    system("git update-index --skip-worktree \"$line\"");
+}
 exit;
+
+sub filesWanted{
+    my $file = $File::Find::name;
+    if ($file =~ /\.suo$/){
+        push @content, $file;
+    }
+    if ($file =~ /\.user$/){
+        push @content, $file;
+    }
+}
