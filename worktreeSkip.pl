962328ff4f01889edaa9e676d84b522700f3c357
Stewart Fraser <stewart.fraser@f-e-t.com>
2018-06-12 11:34:54 +0100

    comments improved

--- worktreeSkip.pl
+++ worktreeSkip.pl
@@ -1,8 +1,13 @@
 #!/usr/bin/perl -w
-# run this after a clean and then a rebuild all
+# steps:
+# 1. get all code from VSS; inital commit select all (including ignored) to git
+# 2. add the template git ignore file for visual studio
+# 3. clean all and then rebuild all
+# 4. run this script to worktree-skip files that building changes (.DLL, .pdb, .log, .txt, .cache)
+#
 # git keeps a record of all files that it is skipping locally ("worktree skip record")
 # run worktreeSkipReverse.pl to remove files from the "worktree skip record"
-
+#
 # runs with Strawberry Perl: http://strawberryperl.com/
 
 use strict;
@@ -13,11 +18,12 @@ use File::Basename;
 use Cwd;
 use POSIX qw(floor);
 
-my @content;
+my @contentSkip;
 my $name;
 my $path;
 my $suffix;
 
+############### add files that have changed since last commit to "worktree skip record" ###############
 my $theFile= "worktreeSkipFiles.txt";
 # list all files that have changed since last commit
 system("git diff --name-only >$theFile");
@@ -33,17 +39,18 @@ foreach my $line (@linesIn) {
     # don't add source files that have changed to the "worktree skip record"
     ($name,$path,$suffix) = fileparse($line,qr"\..[^.]*$");
     if (($suffix eq ".cs") || ($suffix eq ".cpp") || ($suffix eq ".hpp") || ($suffix eq ".h")|| ($suffix eq ".pl") || ($suffix eq ".bat")) {
-        say "WARNING: Source file. Exiting";
+        say "WARNING: Source file ... exiting";
         exit;
     }
     # add file to "worktree skip record"; encase filename / filepath in quotes to handle spaces
     system("git update-index --skip-worktree \"$line\"");
 }
 
+############### user specified files to be added to "worktree skip record" ###############
 # find all files (that user wishes to ignore locally) from current and sub directories
 find( \&filesWanted, '.'); 
 # process each line from the array (add each file to the "worktree skip record")
-foreach my $line (@content) {
+foreach my $line (@contentSkip) {
     chomp($line);
     # remove the "./" at the start of each line (indicating a skipped worktree file)
     my $n=2;
@@ -57,9 +64,12 @@ exit;
 sub filesWanted{
     my $file = $File::Find::name;
     if ($file =~ /\.suo$/){
-        push @content, $file;
+        push @contentSkip, $file;
     }
     if ($file =~ /\.user$/){
-        push @content, $file;
+        push @contentSkip, $file;
+    }
+    if ($file =~ /\.db$/){
+        push @contentSkip, $file;
     }
 }
